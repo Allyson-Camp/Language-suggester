d6c65f74cad43c540c333a476431e12d96db963e
Allyson Camp <Allysoncamp92@gmail.com>
2023-08-11 14:20:59 -0700

    WIP refactoring js & debugging

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,32 @@
-//Business logic
-
-//UI logic
 window.addEventListener("load", function () {
-    const form = document.querySelector("form");
-    const printedAnswer = document.getElementById("printed-answer");
-    printedAnswer.setAttribute("class", "hidden");
-  
-    form.addEventListener("submit", function (e) {
-       e.preventDefault();
-      const singer = document.getElementById("singer").value;
-      const holiday = document.getElementById("holiday").value;
-      const cLang = document.getElementById("c-lang").innerHTML;
-      const jsLang = document.getElementById("js-lang").innerHTML;
-      const reactLang = document.getElementById("react-lang").innerHTML;
-      
-      let result;
-      if (singer === "tay" && holiday  === "halloween") {
-        result = cLang;
-      } else if (singer === "rob" && holiday === "halloween") {
-        result = jsLang;
-      } else if (holiday === "still-halloween"){
-        result = reactLang;
-      } 
-
-      printedAnswer.removeAttribute("class", "hidden");
-      document.getElementById("printed-answer").innerHTML = result;
-    });
-
+  hideAnswer();
+  formEventListener();
 });
+
+  function hideAnswer() {
+  document.getElementById("printed-answer").setAttribute("class", "hidden");
+  }
+  
+function formEventListener() {
+  const form = document.querySelector("form");
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
+        const singer = document.getElementById("singer").value;
+        const holiday = document.getElementById("holiday").value;
+        const cLang = document.getElementById("c-lang").innerHTML;
+        const jsLang = document.getElementById("js-lang").innerHTML;
+        const reactLang = document.getElementById("react-lang").innerHTML;
+
+        let result;
+        if (singer === "tay" && holiday === "halloween") {
+            result = cLang;
+        } else if (singer === "rob" && holiday === "halloween") {
+            result = jsLang;
+        } else if (holiday === "still-halloween") {
+            result = reactLang;
+        }
+
+        document.getElementById("printed-answer").removeAttribute("class", "hidden");
+        document.getElementById("printed-answer").innerHTML = result;
+    });
+}
