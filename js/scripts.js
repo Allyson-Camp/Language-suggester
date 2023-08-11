4a6fc7adb4c5fc14f0ce991df0c2c748c2ce7b67
Allyson Camp <Allysoncamp92@gmail.com>
2023-08-11 14:57:57 -0700

    refactoring js - working

--- js/scripts.js
+++ js/scripts.js
@@ -7,31 +7,35 @@ function showAnswer() {
     document.getElementById("printed-answer").removeAttribute("class", "hidden");
 }
 
-//UI logic
-function formEventListener() {
-    const form = document.querySelector("form");
-    form.addEventListener("submit", function (e) {
-        e.preventDefault();
-        const singer = document.getElementById("singer").value;
-        const holiday = document.getElementById("holiday").value;
-        const cLang = document.getElementById("c-lang").innerHTML;
-        const jsLang = document.getElementById("js-lang").innerHTML;
-        const reactLang = document.getElementById("react-lang").innerHTML;
+function computeAnswer() {
+    const singer = document.getElementById("singer").value;
+    const holiday = document.getElementById("holiday").value;
+    const cLang = document.getElementById("c-lang").innerHTML;
+    const jsLang = document.getElementById("js-lang").innerHTML;
+    const reactLang = document.getElementById("react-lang").innerHTML;
 
-        let result;
-        if (singer === "tay" && holiday === "halloween") {
-            result = cLang;
-        } else if (singer === "rob" && holiday === "halloween") {
-            result = jsLang;
-        } else if (holiday === "still-halloween") {
-            result = reactLang;
-        }
-        document.getElementById("printed-answer").innerHTML = result;
-    });
+    let result;
+    if (singer === "tay" && holiday === "halloween") {
+        result = cLang;
+    } else if (singer === "rob" && holiday === "halloween") {
+        result = jsLang;
+    } else if (holiday === "still-halloween") {
+        result = reactLang;
+    }
+    document.getElementById("printed-answer").innerHTML = result;
+}
+
+
+
+//UI logic
+function formSubmission(event) {
+    event.preventDefault();
+    computeAnswer();
     showAnswer();
 }
 
 window.addEventListener("load", function () {
+    const form = document.querySelector("form");
+    form.addEventListener("submit", formSubmission);
     hideAnswer();
-    formEventListener();
 });
