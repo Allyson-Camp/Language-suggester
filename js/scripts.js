4d0b409fa6c0b60c63985bd00315950d440807a8
Allyson Camp <Allysoncamp92@gmail.com>
2023-08-11 12:03:49 -0700

    clean up js spacing and indentation

--- js/scripts.js
+++ js/scripts.js
@@ -6,15 +6,12 @@ window.addEventListener("load", function () {
 
     form.addEventListener("submit", function (e) {
        e.preventDefault();
-
       const singer = document.getElementById("singer").value;
       const holiday = document.getElementById("holiday").value;
       const cLang = document.getElementById("c-lang").innerHTML;
       const jsLang = document.getElementById("js-lang").innerHTML;
       const reactLang = document.getElementById("react-lang").innerHTML;
-
       let result;
-
       if (singer === "tay" && holiday  === "halloween") {
         result = cLang;
       } else if (singer === "rob" && holiday === "halloween") {
@@ -22,9 +19,7 @@ window.addEventListener("load", function () {
       } else if (holiday === "still-halloween"){
         result = reactLang;
       } 
-
       document.getElementById("printed-answer").innerHTML = result;
-      
     });
   
 });
