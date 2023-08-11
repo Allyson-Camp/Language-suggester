50f6eda0822ac1fb4d9680fde982fbd734dc2e7d
Allyson Camp <Allysoncamp92@gmail.com>
2023-08-11 11:09:38 -0700

    form submit event and branching complete, updates to html <p>s

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,22 @@ window.addEventListener("load", function () {
     const form = document.querySelector("form");
 
     form.addEventListener("submit", function (e) {
-        e.preventDefault();
+       e.preventDefault();
 
-        const singer = document.getElementById("singer").value;
-        const season = document.getElementById("season").value;
+      const singer = document.getElementById("singer").value;
+      const holiday = document.getElementById("holiday").value;
+      const cLang = document.getElementById("c-lang");
+      const jsLang = document.getElementById("js-lang");
+      const reactLang = document.getElementById("react-lang");
+
+      let result;
+
+      if (singer === "tay" && holiday  === "halloween") {
+        result = cLang.removeAttribute("class");
+      } else if (singer === "rob") {
+        result = jsLang.removeAttribute("class");
+      } else {
+        result = reactLang.removeAttribute("class");
+      } 
     });
 });
